d37891a940f02fdd64d53250de02a1522d3a1426
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2025-08-08 14:04:20 +0300

    fixed component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,5 +1,6 @@
 export const Tabs = ({ tabs, onTabSelected, activeTabId }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
@@ -29,7 +30,7 @@ export const Tabs = ({ tabs, onTabSelected, activeTabId }) => {
       </div>
 
       <div className="block" data-cy="TabContent">
-          {activeTab.content}
+        {activeTab.content}
       </div>
     </div>
   );
